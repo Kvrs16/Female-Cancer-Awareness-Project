3189a4a570ede89f64176217c60c73384a38f6fb
Kvrs16 <toomuchhappy1005@gmail.com>
2024-01-23 23:53:03 +0530

    add files

---
+++ assets/js/faq.js
@@ -0,0 +1,17 @@
+
+// scroll revela js starts here 
+ScrollReveal().reveal("#headingOne", {delay:300});
+ScrollReveal().reveal("#headingTwo", {delay:350});
+ScrollReveal().reveal("#headingThree", {delay:400});
+ScrollReveal().reveal("#headingFour", {delay:450});
+ScrollReveal().reveal("#headingFive", {delay:500});
+ScrollReveal().reveal("#headingSix", {delay:550});
+ScrollReveal().reveal("#headingSeven", {delay:600});
+ScrollReveal().reveal("#headingEight", {delay:300});
+ScrollReveal().reveal("#headingNine", {delay:300});
+ScrollReveal().reveal("#headingTen", {delay:300});
+ScrollReveal().reveal("#headingEleven", {delay:300});
+ScrollReveal().reveal("#headingTwelve", {delay:300});
+ScrollReveal().reveal("#headingThirteen", {delay:300});
+ScrollReveal().reveal("#headingFourteen", {delay:300});
+// scroll revela js ends here
